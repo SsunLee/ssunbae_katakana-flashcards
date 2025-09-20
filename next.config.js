6d57713b1b8162ddbb513faeddc72feb0a1a5a1e
SsunLee <tnsqo1126@naver.com>
2025-09-20 23:31:16 +0900

    native app setup

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
+// app/next.config.js
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ▼▼▼ 이 옵션을 추가해주세요 ▼▼▼
   output: 'export',
 
   // (기존에 다른 설정이 있었다면 그대로 둡니다)
 };
 
-module.exports = nextConfig;
+// module.exports = nextConfig; 대신 아래 코드를 사용합니다.
+export default nextConfig;
+
